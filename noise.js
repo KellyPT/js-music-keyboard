a73fbc69173d35ed3f5031006e2690760c8454d5
KellyPT <trankphuong@gmail.com>
2016-11-21 21:59:49 -0800

    DRY code.

--- noise.js
+++ noise.js
@@ -1,17 +1,18 @@
+var playSound = function(key) {
+  var audio = "#" + key + "Audio";
+  var sound = $(audio)[0];
+  sound.load();
+  sound.play();
+};
+
 $(document).ready( function() {
   $('.instrument').on('click', 'button', function(event) {
     var key1 = $(this).html();
-    var audio1 = "#" + key1 + "Audio";
-    var v1 = $(audio1)[0];
-    v1.load();
-    v1.play();
+    playSound(key1);
   });
 
   $(document).keydown(function(event) {
     var key2 = event.key;
-    var audio2 = "#" + key2 + "Audio";
-    var v2 = $(audio2)[0];
-    v2.load();
-    v2.play();
+    playSound(key2);
   });
 });
